bfc14dcb9cfaa09382e1a6966047db5969600bb9
Rachel-Chloe Gregory <rachel.chloe.gregory@gmail.com>
2019-08-22 19:22:29 -0500

    Added comments

--- pybank/main2.py
+++ pybank/main2.py
@@ -1,5 +1,9 @@
 import os
 import csv
+import time
+
+# For calculating script runtime
+start = time.time()
 
 # Path to collect data from the Resources folder
 data_csv = "Homework 3_Instructions_PyBank_Resources_budget_data.csv"
@@ -10,10 +14,11 @@ profit_change_list = []
 greatest_profit_change = 0
 worst_profit_change = 0
 
-# Define the function and have it accept the 'wrestlerData' as its sole parameter
+# Define the function and have it accept each row from our file
 def data_analysis(data):
     month = data[0]
     profit = data[1]
+    # Return values of these indexes
     return(month,profit)
     
 # Read in the CSV file
@@ -22,28 +27,45 @@ with open(data_csv, 'r') as csvfile:
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
 
+    # Skip the header
     csv_header = next(csvreader)
 
     # Loop through the data
     for row in csvreader:
+        # Take the output from our function
         month,profit = data_analysis(row)
+        # Count total months
         months += 1
+        # Sum total profit
         total_profit = total_profit + int(profit)
+        # If we're at the first row, set initial values
         if row[0] == 'Jan-2010':
             profit_change = 0
             profit_change_list.append(profit_change)
+        # If we're at any other row, run rolling calculations
         else:
             profit_change = int(profit) - previous_profit
             profit_change_list.append(profit_change)
+        # Get greatest and worst profit change with comparison
         if profit_change > greatest_profit_change:
             greatest_profit_change = profit_change
             greatest_profit_change_month = month
         elif profit_change < worst_profit_change:
             worst_profit_change = profit_change
             worst_profit_change_month = month
+        # Reset previous_profit
         previous_profit = int(profit)
+    # Calculate the average change over time
     avg_change = sum(change for change in profit_change_list)/(len(profit_change_list)-1)
+    # Round the average change
     avg_change = round(avg_change,2)
+
+    # Print and write our data to a file
     print (f'Financial Analysis\n----------------------------\nTotal Months: {months}\nTotal: ${total_profit}\nAverage Change: ${avg_change}\nGreatest Increase in Profits: {greatest_profit_change_month} (${greatest_profit_change})\nGreatest Decrease in Profits: {worst_profit_change_month} (${worst_profit_change})')
     output_file = open("HWFile.txt","w")
     output_file.write(f'Financial Analysis\n----------------------------\nTotal Months: {months}\nTotal: ${total_profit}\nAverage Change: ${avg_change}\nGreatest Increase in Profits: {greatest_profit_change_month} (${greatest_profit_change})\nGreatest Decrease in Profits: {worst_profit_change_month} (${worst_profit_change})')
+
+end = time.time()
+# time to run: 0.0004928112030029297
+# This one's super fast
+print(end - start)
--- pypoll/main.py
+++ pypoll/main.py
@@ -1,5 +1,9 @@
 import os
 import csv
+import time
+
+# For calculating script runtime
+start = time.time()
 
 data_csv = "Homework 3_Instructions_PyPoll_Resources_election_data.csv"
 total_votes = 0
@@ -19,36 +23,50 @@ with open(data_csv, 'r') as csvfile:
 
     # Loop through the data
     for row in csvreader:
+        # Add to votes each time
         total_votes += 1
+        # If there is a new candidate, add them
         if row[2] not in candidates:
             candidates.append(row[2])
             votes = 1
             candidate_votes.update({row[2]:votes})
+        # If we've seen the candidate before, add to their tally
         else:
             votes = candidate_votes[row[2]] + 1
             candidate_votes.update({row[2]:votes})
-    
+    # For each candidate, calculate their percentage of the votes
     for key in candidate_votes:
         percent = round(candidate_votes[key]/total_votes*100,2)
+        # Make a table of the candidates and percents
         table.update({key:percent})
+        # Find whoever has the greatest percentage of votes
         if candidate_votes[key]>winner_votes:
             winner_votes = candidate_votes[key]
             winner = key
 
+    # Create a list of our formatted results
     formatted_data.append("\nElection Results")
     formatted_data.append("-------------------------")
     formatted_data.append("Total Votes: " + str(total_votes))
     formatted_data.append("-------------------------")
 
+    # For each candidate, build a string with their results
     for candidate in candidates:
         print_string = str(candidate) + ": " + str(table[candidate]) + "% ("+ str(candidate_votes[candidate])+")"
         formatted_data.append(print_string)
 
+    # Add candidate results to the results list
     formatted_data.append("-------------------------")
     formatted_data.append("Winner: "+winner)
     formatted_data.append("-------------------------\n")
 
+    # Print and export our formatted data
     output_file = open("HWFile.txt","w")
     for x in range(len(formatted_data)):
         print(formatted_data[x])
         output_file.write(formatted_data[x]+"\n")
+
+end = time.time()
+# time to run: 3.885343074798584
+# This one takes forever!
+print(end - start)
